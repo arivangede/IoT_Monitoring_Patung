603995632f193d1f691ec0d11e9dfef44fe76b7d
Arivan G. Eka Satria <arivan.gede@gmail.com>
2024-10-13 15:22:51 +0800

    first commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div
+        class="min-h-screen w-full flex flex-col justify-center items-center bg-slate-600 bg-img relative z-0"
+    >
+        <!-- <h1 class="font-bold text-2xl text-primary z-10">
+            CleanStatue Monitor
+        </h1> -->
+        <div class="z-10">
+            <slot class="z-10" />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import AuthForm from "@/Components/Auth/AuthForm.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Login" />
+
+    <GuestLayout>
+        <div
+            class="flex flex-col justify-center items-center w-full max-w-screen-lg"
+        >
+            <AuthForm>
+                <h1 class="font-semibold text-xl">Login</h1>
+
+                <div>
+                    <label for="username" class="ml-3">Username</label>
+                    <input
+                        type="text"
+                        placeholder="Username / Email"
+                        class="input input-bordered w-full max-w-sm"
+                    />
+                </div>
+                <div>
+                    <label for="password" class="ml-3">Password</label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        class="input input-bordered w-full max-w-sm"
+                    />
+                </div>
+            </AuthForm>
+
+            <p class="text-primary">
+                Belum Punya Akun?
+                <Link href="#" class="underline font-semibold"
+                    >Daftar Sekarang</Link
+                >
+            </p>
+        </div>
+    </GuestLayout>
+</template>
