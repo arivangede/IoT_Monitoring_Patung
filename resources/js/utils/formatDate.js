0875cf80b9ceccb769296a586ba5ad836126b7f9
Arivan G. Eka Satria <arivan.gede@gmail.com>
2025-02-05 10:47:02 +0800

    fix bug on format date

--- resources/js/utils/formatDate.js
+++ resources/js/utils/formatDate.js
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
 import "dayjs/locale/id";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 dayjs.locale("id");
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default function formatDate(date, format = "D MMM YYYY HH:mm") {
-    return dayjs(date).format(format);
+    return dayjs(date).utc(true).tz("Asia/Makassar").format(format);
 }
